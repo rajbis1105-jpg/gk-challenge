931219e53958f306ccf4abc19b8a740688b6292b
rajbis1105-jpg <rajbis1105@gmail.com>
2026-07-24 18:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request
 from questions import questions
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 @app.route("/quiz", methods=["GET", "POST"])
 def quiz():
-
-    page = int(request.args.get("page", 1))
-
-    start = (page - 1) * 10
-    end = start + 10
-
-    current_questions = questions[start:end]
 
     if request.method == "POST":
 
@@ -17,17 +18,20 @@ def quiz():
         results = []
 
         for i, q in enumerate(questions):
-            user_answer = request.form.get(f"q{i}")
 
-            is_correct = user_answer == q["answer"]
+            user_answer = request.form.get(f"q{i}")
+            correct_answer = q["answer"]
+
+            is_correct = user_answer == correct_answer
 
             if is_correct:
                 score += 1
 
             results.append({
                 "question": q["question"],
+                "options": q["options"],
                 "user_answer": user_answer if user_answer else "No Answer",
-                "correct_answer": q["answer"],
+                "correct_answer": correct_answer,
                 "is_correct": is_correct
             })
 
@@ -40,7 +44,9 @@ def quiz():
 
     return render_template(
         "quiz.html",
-        questions=current_questions,
-        page=page,
-        total_pages=(len(questions) + 9) // 10
+        questions=questions
     )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
